55320e8395c4ac1d4e7e23fa14c48d65f6feea2f
Ann Tech <anntech21@gmail.com>
2023-10-14 18:25:38 -0700

    updated assignment 1

--- scripts.js
+++ scripts.js
@@ -1,27 +1,24 @@
 /************************ Exercise 1 - Identify Variables ************************/
 
 // Declare Variables Here
-let login 
-let totalSales
-let totalExpenses 
-let totalProfits
-let customerDemograph
-let salesBreakdown
-let ageGroup
-let adSpent
-let Apps
-let employees
-let upsell
-let bundles
-let abandantCar
-let 18_26
-let 27_45
-let 45_65
-let locations
-let boston
-let newyork
-let california
-let notification
+let login;
+let totalSales;
+let totalExpenses ;
+let totalProfits;
+let customerDemograph;
+let salesBreakdown;
+let ageGroup;
+let adSpent;
+let Apps;
+let employees;
+let upsell;
+let bundles;
+let abandantCar;
+let locations;
+let boston;
+let newyork;
+let california;
+let notification;
 
 
 
@@ -35,9 +32,9 @@ let age = "39";
 
 
 // Log the variables' values to the console.
-console.log ("firstName");
-console.log ("lastName");
-console.log ("age");
+console.log (firstName);
+console.log (lastName);
+console.log (age);
 
 
 
@@ -48,33 +45,41 @@ console.log ("age");
 // Create two variables, a and b, and assign them any values.
 
 let a = "colin";
+console.log (a);
 let b = "catie";
+console.log (b);
+
 
 // Swap the values of a and b. You may use a third variable to hold a value so you don't overwrite it. Use reassignment—don't change the previous lines of code!
+let c = "laura";
+console.log (c);
+
 a = b;
-let c = a;
+console.log (a);
 b = c;
 
 
 // Log the values of a and b before and after the swap.
 //log before
-console.log ("a");//colin
-console.log ("b");//catie
-//log after
-console.log('Lets Swap! ' + a + ', use to be colin and catie is now ' + b +'.' )//catie use to be colin and catie is now colin
+//console.log (a);//colin
+//console.log (b);//catie
+//console.log (c); // Laura
 
+//log after
+console.log('Lets Swap! ' + a + ', use to be colin and catie is now ' + b +'.' )
+Lets Swap! catie, use to be colin and catie is now laura.
 
 
 /************************ Exercise 4 - Working with Constants ************************/
-.
+
 // Declare a constant variable named PI and set its value to 3.14159. 
 const PI = 3.14159
 
 // Then, attempt to reassign a new value to PI. 
-PI = 5245
+//PI = 5245
 
 // Observe the error in the console and write a comment here explaining why you can't reassign a constant.
-constant is permantant and can not change value
+constant is permantant and can not change value 
 
 
 let text ='Hello';
